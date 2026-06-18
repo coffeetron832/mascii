39cdfc76809e7ff8bd6fa05e7a38bc2767e0f1f4
coffeetron832 <jpontonh@gmail.com>
2026-06-18 12:06:23 -0500

    cambios045

--- core/version.js
+++ core/version.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const packagePath = path.join(__dirname, "package.json");
+const packagePath = path.join(__dirname, "..", "package.json");
 
 const packageJson = JSON.parse(
   fs.readFileSync(packagePath, "utf8")
